69b12f39e5e3f70142f44f6fde6530a984332cd7
krama99 <45354937+krama99@users.noreply.github.com>
2023-10-18 19:06:06 +0530

    Update hellojee.java
    hgjh

--- hellojee.java
+++ hellojee.java
@@ -3,6 +3,6 @@ import com.sun.swing.internal.plaf.synth.resources.synth_sv;
 public class hellojee {
 
    public static void main(String [] args) {
-       System.out.println("hello");
+       System.out.println("hellokkkkkk");
    }
 }
